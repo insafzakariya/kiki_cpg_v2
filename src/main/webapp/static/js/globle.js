82b25121b28975d43360a7a787be05b4ec73d174
Anjana <anjanathrishakya@gmail.com>
2020-09-30 17:05:24 +0530

    change globle js base URL

--- src/main/webapp/static/js/globle.js
+++ src/main/webapp/static/js/globle.js
@@ -4,7 +4,7 @@
 
 //const baseURL = "http://localhost:8081/susilawebpay";
 //const baseURL = "https://payv2.kiki.lk/susilawebpay";
-const baseURL = "http://payment.kiki.lk/susilawebpay";
+const baseURL = "https://payment.kiki.lk/susilawebpay";
 const paymentGatewayRedirectURL = "https://payv2.kiki.lk/webpay-hnb/web/payment_form.jsp?";
 //const paymentGatewayRedirectURL = "http://localhost:8080/webpay-hnb/web/payment_form.jsp?";
 //const baseURL = "http://35.200.234.252:8080/susilawebpay_test";
